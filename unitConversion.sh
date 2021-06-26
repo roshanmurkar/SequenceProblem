8031e7c39bef8416a7eaf79f252432b9cb969687
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 14:40:27 +0530

    area and volume

---
+++ unitConversion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+l=60;
+b=40;
+
+area=$(($l*$b))
+
+volume=`expr $area | awk '{volume=$1/3.281} {print volume}'`
+echo "length of rectangular plot is:- " $l "feet & breadth is " $b "feet"
+echo "area of rectangular plot is :- " $area "meters"
