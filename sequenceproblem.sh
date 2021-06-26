26afc049a79a8fa2726d52d7bc039a5daa0490b4
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-06-26 14:22:19 +0530

    addition and average

--- sequenceproblem.sh
+++ sequenceproblem.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -x
 
-# add two random dice
+# 5 digit addition and avg
 
-diceNum1=$((RANDOm%6 +1))
-diceNum2=$((RANDOM%6 +1))
+num1=$((RANDOM%100))
+num2=$((RANDOM%100))
+num3=$((RANDOM%100))
+num4=$((RANDOM%100))
+num5=$((RANDOM%100))
 
-result=$(( $diceNum1 + $diceNum2 ))
-echo $result
+sum=$(( $num1 + $num2 + $num3 + $num4 + $num5 ))
+echo "total sum of 2 digit 5 random numbers is:" $sum
+
+avg=`expr $sum | awk '{avg=$1/5} {print avg}'`
+echo " the avd of 2 digit  5 random number is:-" $avg
